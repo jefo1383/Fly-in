a7474cf1a8326433f4305f16bd04c98f22a3f428
jfoeller <jeremy.foeller@learner.42.tech>
2026-07-20 16:08:18 +0200

    classe drone

--- drone_map_models.py
+++ drone_map_models.py
@@ -4,7 +4,54 @@
 from hub_link_models import NormalHub, PriorityHub, RestrictedHub, \
     BlockedHub, StartHub, EndHub, Link, Hub
 from typing import Any, cast
-from parsing import LinkData, HubData
+from parsing import HubData
+
+
+class Drone:
+    """Represents a drone navigating the network.
+
+    Attributes:
+        current_pos (Hub | Link): The current node or edge.
+        # TODO: Define additional attributes.
+    """
+
+    def __init__(self, drone_id: str, start_hub: StartHub,
+                 end_hub: EndHub) -> None:
+        """Initializes a Drone with an ID and starting position.
+
+        Args:
+            drone_id (str): The unique identifier for the drone.
+            start_hub (Hub): The initial hub where the drone is spawned.
+            end_hub (Hub): The final hub to reach.
+        """
+        self.drone_id = drone_id
+        self.start_hub = start_hub
+        self.end_hub = end_hub
+        self.current_pos: Hub | Link = start_hub
+        self.is_arrived: bool = self.current_pos == self.end_hub
+        self.turns_remaining: int = 0
+
+    def move(self, target: Hub | Link, wait_time: int = 0) -> None:
+        """Moves the drone to the specified target.
+
+        Handles the logic for updating the drone's current position,
+        decreasing wait times if currently on a link, and updating
+        the arrival status.
+
+        Args:
+            target (Hub | Link): The destination hub or the link to
+                transit on.
+            wait_time (int): the number of turns to wait.
+        """
+        if self.is_arrived:
+            return
+        if self.turns_remaining > 0 and isinstance(self.current_pos, Link):
+            self.turns_remaining -= 1
+        else:
+            self.current_pos = target
+            self.turns_remaining = wait_time
+            if self.current_pos == self.end_hub:
+                self.is_arrived = True
 
 
 class Map:
@@ -42,7 +89,33 @@ class Map:
             self.hubs[end_hub.name] = end_hub
             self.adjacency[end_hub.name] = []
         for hub_data in config.get("hubs", []):
-            pass
+            new_hub: Hub
+            match hub_data.zone:
+                case "normal":
+                    new_hub = NormalHub(
+                        hub_data.name, hub_data.x, hub_data.y,
+                        hub_data.color, hub_data.max_drones
+                    )
+                case "restricted":
+                    new_hub = RestrictedHub(
+                        hub_data.name, hub_data.x, hub_data.y,
+                        hub_data.zone, hub_data.color, hub_data.max_drones
+                    )
+                case "priority":
+                    new_hub = PriorityHub(
+                        hub_data.name, hub_data.x, hub_data.y,
+                        hub_data.zone, hub_data.color, hub_data.max_drones
+                    )
+                case "blocked":
+                    new_hub = BlockedHub(
+                        hub_data.name, hub_data.x, hub_data.y,
+                        hub_data.zone, hub_data.color
+                    )
+                case _:
+                    raise ValueError(f"Invalid zone type detected: \
+                                     {hub_data.zone}")
+            self.hubs[new_hub.name] = new_hub
+            self.adjacency[new_hub.name] = []
 
     def _build_links(self, config: dict[str, Any]) -> None:
         """Instantiates Link objects and builds the adjacency list.
@@ -51,4 +124,16 @@ class Map:
             config (dict[str, Any]): The parsed configuration.
         """
         for link_data in config.get("connections", []):
-            pass
+            if (
+                link_data.hub_1 in self.hubs and
+                link_data.hub_2 in self.hubs
+            ):
+                new_link = Link(self.hubs[link_data.hub_1],
+                                self.hubs[link_data.hub_2],
+                                link_data.max_link_capacity)
+                self.adjacency[new_link.hub_1.name].append(new_link)
+                self.adjacency[new_link.hub_2.name].append(new_link)
+            else:
+                raise ValueError(f"Invalid connection detected: "
+                                 f"{link_data.hub_1} or {link_data.hub_2}"
+                                 f"doesn't exist")
